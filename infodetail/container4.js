c4f37522c3b236b4475a5cc7f1dcc9215cbb0083
gwonhyeyeon <hyeyun0414@naver.com>
2023-03-07 12:26:03 +0900

    다시

--- infodetail/container4.js
+++ infodetail/container4.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
     $(".content1").hide();
     $(".title1").click(function(){
-        $(".content1").slideToggle(500);
+        $(".content1").slideToggle(600);
     });
 });
 // background-image: url('porsche-normal(7).jpeg');
@@ -9,22 +9,15 @@ $(document).ready(function(){
 $(document).ready(function(){
     $(".content2").hide();
     $(".title2").click(function(){
-        $(".content2").slideToggle(500);
+        $(".content2").slideToggle(600);
     });
 });
 
 $(document).ready(function(){
     $(".content3").hide();
     $(".title3").click(function(){
-        $(".content3").slideToggle(500);
+        $(".content3").slideToggle(600);
     });
 });
 
 
-
-// var myBtn = $('.title1');
-// var img = container4.find('#image');
-
-// myBtn.on('click', function(){
-//     img.css({backgroundImage : 'porsche_coupe.jpeg'})
-// })
