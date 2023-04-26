1b834973876215a07afc4d7d287e50dcfea85f1b
juca <dhzlehzl01@naver.com>
2023-04-26 22:19:30 +0900

    update: login & signpage

--- 2nd-project/src/App.js
+++ 2nd-project/src/App.js
@@ -1,16 +1,21 @@
 import { Reset } from 'styled-reset';
 import './App.css';
-import Header from './components/Header';
-import Login from './components/Login';
-import Footer from './components/Footer';
+import { Route, Routes } from 'react-router-dom';
+import LoginPage from './pages/LoginPage.js';
+import SignupPage1 from './pages/SignupPage/SignupPage1';
+import SignupPage2 from './pages/SignupPage/SignupPage2';
+import SignupPage3 from './pages/SignupPage/SignupPage3';
 
 function App() {
   return (
     <div className="App">
       <Reset />
-      <Header />
-      <Login />
-      <Footer />
+      <Routes>
+        <Route index path='/' element={<LoginPage />}/>
+        <Route path='/signup1' element={<SignupPage1 />} />
+        <Route path='/signup2' element={<SignupPage2 />} />
+        <Route path='/signup3' element={<SignupPage3 />} />
+      </Routes>
     </div>
   );
 }
--- 2nd-project/src/components/Footer.js
+++ 2nd-project/src/components/Footer.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import './Footer.css'
+import '../css/Footer.css'
 
 function Footer() {
   return (
@@ -16,6 +16,19 @@ function Footer() {
             <li>미환급금조회</li>
             <li>명의도용방지서비스</li>
             <li>장애현황</li>
+          </ul>
+
+          <div className='adress'>
+            (주)엘지유플러스 서울특별시 용산구 한강대로 32 대표이사 황현식 사업자 등록번호 220-81-39938 통신판매신고 제 2015-서울용산-00481호 사업자정보확인 &gt; 
+          </div>
+
+          <ul className='tel'>
+            <li>고객센터 휴대폰 114(무료) 1544-0010(유료)</li>
+            <li>인터넷/IPTV/전화 101(무료)</li>
+            <li>가입문의 휴대폰 1644-7009(유료)</li>
+            <li>인터넷/IPTV/전화 1644-7070(유료)</li>
+            <li>스마트홈 1544-0107(유료)</li>
+            <li>소상공인 인터넷/IPTV/전화 1800-8000(유료)</li>
           </ul>
         </div>
       </div>
